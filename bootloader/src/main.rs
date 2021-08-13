f994df1b97b5d69fec1304dbb330c9c1c1c852bc
Nanamiiiii <akihiro_0605@outlook.com>
2021-08-13 23:40:02 +0900

    Load Kernel

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -4,11 +4,13 @@
 #![feature(asm)]
 
 use uefi::prelude::*;
+// use uefi::proto::console::gop::FrameBuffer;
 use uefi::proto::loaded_image::LoadedImage;
 use uefi::proto::media::fs::SimpleFileSystem;
 use uefi::proto::media::file::{File, RegularFile, Directory, FileMode, FileAttribute, FileType, FileInfo};
-use uefi::table::boot::MemoryType;
+use uefi::table::boot::{AllocateType, MemoryType};
 use uefi::data_types::Align;
+use core::mem;
 use core::slice::from_raw_parts_mut;
 use core::panic::PanicInfo;
 use elf_rs::*;
@@ -103,12 +105,13 @@ pub extern "C" fn efi_main(_handle: Handle, system_table: SystemTable<Boot>) -> 
         .unwrap_success();
     let _kernel_file_size: u64 = kernel_info.file_size();
     const KERNEL_BASE_ADDR: usize = 0x100000;
+    const PAGE_SIZE: usize = 0x1000;
 
     // Load kernel to memory pool
     let tmp_kernel_ptr = bs.allocate_pool(
-        MemoryType::LOADER_DATA, 
-        (_kernel_file_size as usize + 0xfff) / 0x1000)
-        .unwrap_success();
+            MemoryType::LOADER_DATA, 
+            (_kernel_file_size as usize + 0xfff) / PAGE_SIZE
+        ).unwrap_success();
 
     let tmp_kernel_buf: &mut [u8] = unsafe { from_raw_parts_mut(tmp_kernel_ptr as *mut u8, _kernel_file_size as usize) }; 
     let _kernel_read_size = kernel_file.read(tmp_kernel_buf).unwrap_success();
@@ -123,21 +126,56 @@ pub extern "C" fn efi_main(_handle: Handle, system_table: SystemTable<Boot>) -> 
     };
     let mut kernel_st: u64 = core::u64::MAX;
     let mut kernel_ed: u64 = 0;
-    writeln!(system_table.stdout(), "{:?} {:?}", e, e.header()).unwrap();
+    // writeln!(system_table.stdout(), "{:?} {:?}", e, e.header()).unwrap();
     for _header_iter in e.program_header_iter() {
-        writeln!(system_table.stdout(), "{:x?}", _header_iter).unwrap();
+        // writeln!(system_table.stdout(), "{:x?}", _header_iter).unwrap();
         let header = _header_iter.ph;
         if header.ph_type() == ProgramType::LOAD {
             kernel_st = core::cmp::min(kernel_st, header.vaddr());
-            kernel_ed = core::cmp::max(kernel_ed, header.memsz());
+            kernel_ed = core::cmp::max(kernel_ed, header.vaddr() + header.memsz());
         }
+    }#[allow(clippy::empty_loop)]
+
+    writeln!(system_table.stdout(), "Kernel: 0x{:x} ({} bytes) {:p} - {:p}", KERNEL_BASE_ADDR, _kernel_file_size, kernel_st as *const u8, kernel_ed as *const u8).unwrap();
+
+    let kernel_size = kernel_ed as usize - kernel_st as usize;
+    let page_count = (kernel_size + PAGE_SIZE - 1) / PAGE_SIZE;
+    let page_addr = bs.allocate_pages(
+            AllocateType::Address(KERNEL_BASE_ADDR),
+            MemoryType::LOADER_DATA,
+            page_count
+        ).unwrap_success();
+
+    for _header_iter in e.program_header_iter() {
+        let ph = _header_iter.ph;
+        if ph.ph_type() != ProgramType::LOAD {
+            continue;
+        }
+        let offset = ph.offset() as usize;
+        let filesz = ph.filesz() as usize;
+        let memsz = ph.memsz() as usize;
+        let destination = unsafe {
+            from_raw_parts_mut(ph.vaddr() as *mut u8, memsz)
+        };
+        destination[..filesz].copy_from_slice(&tmp_kernel_buf[offset..offset + filesz]);
+        destination[filesz..].fill(0);
     }
 
-    writeln!(system_table.stdout(), "Kernel: 0x{:x} ({} bytes)", KERNEL_BASE_ADDR, _kernel_file_size).unwrap();
+    let kernel: &mut [u8] = unsafe { from_raw_parts_mut(page_addr as *mut u8, kernel_size) };
+
+    type EntryPoint = extern "sysv64" fn() -> ();
+    let entry = {
+        const ENTRY_OFFSET: usize = 24;
+        let entry_addr = kernel.as_ptr().wrapping_add(ENTRY_OFFSET) as *const u64;
+        unsafe { mem::transmute::<u64, EntryPoint>(entry_addr.read()) }
+    };
 
     // Exit boot service
     let (_systable_runtime, _descriptor_itr) = system_table.exit_boot_services(_handle, &mut memorymap_buffer).unwrap_success();
  
+    entry();
+
+    #[allow(clippy::empty_loop)]
     loop {}
     
 }
